ee647e33f5ff69f5a1bf0be138bfcab5a6ddf16b
Steven Casper <sebastiancasper3@gmail.com>
2023-03-13 13:35:42 -0700

    Update price on startup

--- src/mod.ts
+++ src/mod.ts
@@ -1,20 +1,46 @@
 import { DependencyContainer } from "tsyringe";
-import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
+
+import { IPostDBLoadModAsync } from "@spt-aki/models/external/IPostDBLoadModAsync";
+import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
+
 import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
 import { LogTextColor } from "@spt-aki/models/spt/logging/LogTextColor";
 import { LogBackgroundColor } from "@spt-aki/models/spt/logging/LogBackgroundColor";
+import { get } from "https";
 
-class Mod implements IPostDBLoadMod
-{
-    postDBLoad(container: DependencyContainer): void 
-    {
-        // get the logger from the server container
+class Mod implements IPostDBLoadModAsync {
+    public async postDBLoadAsync(container: DependencyContainer): Promise<void> {
         const logger = container.resolve<ILogger>("WinstonLogger");
-        
-        logger.info("I am logging info!");
-        logger.warning("I am logging a warning!");
-        logger.error("I am logging an error!");
-        logger.logWithColor("I am logging with color!", LogTextColor.YELLOW, LogBackgroundColor.RED);
+        const db = container.resolve<DatabaseServer>("DatabaseServer");
+
+        const price = await this.getPrice();
+        const tables = db.getTables();
+        const handbook = tables.templates.handbook;
+        const bitcoin = handbook.Items.find(x => x.Id == "59faff1d86f7746c51718c9c");
+        const usd = handbook.Items.find(x => x.Id == "5696686a4bdc2da3298b456a");
+
+        const inRub = price * usd.Price * 0.18;
+        logger.logWithColor(`updating bitcoin to ${inRub}`, LogTextColor.MAGENTA, LogBackgroundColor.WHITE);
+        bitcoin.Price = inRub;
+    }
+
+    private async getPrice(): Promise<number> {
+        return new Promise((resolve, reject) => get(
+            "https://api.blockchain.com/v3/exchange/tickers/BTC-USD",
+            (res) => {
+                res.setEncoding("utf8");
+                let rawData = "";
+                res.on("data", (chunk) => { rawData += chunk; });
+                res.on("end", () => {
+                    try {
+                        const parsedData = JSON.parse(rawData);
+                        const price = parseInt(parsedData["price_24h"], 10);
+                        resolve(price);
+                    } catch (e) {
+                        console.error(e.message);
+                    }
+                });
+            }));
     }
 }
 
